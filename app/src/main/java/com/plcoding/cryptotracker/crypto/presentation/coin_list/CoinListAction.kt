d02d309f405f787eea1d9c3debbfa43b2a1c3aea
krisnadibyo <krisna_itbstei@hotmail.com>
2025-04-18 12:33:00 +0700

    setup di

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListAction.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListAction.kt
@@ -4,4 +4,5 @@ import com.plcoding.cryptotracker.crypto.presentation.models.CoinUi
 
 sealed interface CoinListAction {
     class OnCoinClick(val coinUi: CoinUi) : CoinListAction
+    data object OnRefresh: CoinListAction
 }
